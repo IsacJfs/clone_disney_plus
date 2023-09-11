7acab76f4690406d3ddd790dd572fdcdeed9e792
IsacJfs <isac.jfs@gmail.com>
2023-09-11 11:27:25 -0300

    :bookmark: implementação da minificação do styles, scripts e images

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'))
 }
 function images() {
@@ -12,7 +14,13 @@ function images() {
         .pipe(gulp.dest('./dist/images'))
 }
 
-exports.default = gulp.parallel(styles, images);
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
